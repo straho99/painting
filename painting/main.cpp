46ffd38538bf820d5e53227559aeb429f9d4de58
StrahilRuychev <strahil.ruychev@gmail.com>
2024-02-20 18:29:30 +0200

    Initial solution

--- painting/main.cpp
+++ painting/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  painting
-//
-//  Created by Strahil Ruychev on 20.02.24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int fence_length;
+    cin >> fence_length;
+    
+    int painted_fence[fence_length];
+    
+    for (int i = 0; i < fence_length; i++) {
+        cin >> painted_fence[i];
+    }
+    
+    int used_colors = 0, used_quantity = 0;
+    
+    for (int i = 0; i < fence_length; i++) {
+        if (painted_fence[i] == 0) {
+            continue;
+        }
+        
+        if (used_colors == 0) {
+            used_colors++;
+            used_quantity++;
+            continue;
+        }
+        
+        if (painted_fence[i] != painted_fence[i-1]) {
+            used_colors++;
+        }
+        
+        used_quantity++;
+    }
+    
+    cout << used_colors << " " << used_quantity << endl;
+    
     return 0;
 }
